b7698496bcc7edc4415c672a42f3da11f389b1d7
Sebas-gifPaz777 <pazsebas797@gmail.com>
2025-05-18 20:44:56 -0500

    Configurations done

--- src/main/java/com/futurex/services/FutureXCourseApp/CourseRepository.java
+++ src/main/java/com/futurex/services/FutureXCourseApp/CourseRepository.java
@@ -1,8 +1,10 @@
 package com.futurex.services.FutureXCourseApp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, BigInteger> {
 }
